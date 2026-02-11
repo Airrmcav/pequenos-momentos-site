c2c74afd8a281e40d8debcc98e5501ec159b5be4
Axel Ivan Ramírez Reséndiz <ivanramres@gmail.com>
2026-02-11 10:47:42 -0600

    fix:ContentType

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,27 +1,29 @@
 import { defineCollection, z } from "astro:content";
 
 const servicios = defineCollection({
+  type: 'content',
   schema: z.object({
     title: z.string(),
-    shortTitle: z.string(),
+    shortTitle: z.string().optional(),
     description: z.string(),
-    imageBanner: z.string(),
-    video: z.string(),
-    categoria: z.string(),
+    imageBanner: z.string().optional(),
+    video: z.string().optional(),
+    categoria: z.string().optional(),
     images: z.array(
       z.object({
         src: z.string(),
-        alt: z.string(),
-        title: z.string(),
+        alt: z.string().optional(),
+        title: z.string().optional(),
       })
-    ),
+    ).optional(),
     content: z.string().optional(),
-    url: z.string(),
-    icon: z.string(),
+    url: z.string().optional(),
+    icon: z.string().optional(),
   }),
 });
 
 const paquetes = defineCollection({
+  type: 'content',
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -36,14 +38,15 @@ const paquetes = defineCollection({
 });
 
 const eventos = defineCollection({
+  type: 'content',
   schema: z.object({
     id: z.number(),
     nombre: z.string(), 
     tipoServicio: z.string(),
     fecha: z.string(),
     ubicacion: z.string(),
-    video: z.string(), 
-    imagen: z.string(), 
+    video: z.string().optional(), 
+    imagen: z.string().optional(), 
     descripcion: z.string(), 
     serviciosIncluidos: z.array(z.string()), 
     duracion: z.string(), 
@@ -52,22 +55,23 @@ const eventos = defineCollection({
 });
 
 const actividades = defineCollection({
+  type: 'content',
   schema: z.object({
     title: z.string(),
-    shortTitle: z.string(),
+    shortTitle: z.string().optional(),
     description: z.string(),
-    imageBanner: z.string(),
-    video: z.string(),
+    imageBanner: z.string().optional(),
+    video: z.string().optional(),
     images: z.array(
       z.object({
         src: z.string(),
-        alt: z.string(),
-        title: z.string(),
+        alt: z.string().optional(),
+        title: z.string().optional(),
       })
-    ),
+    ).optional(),
     content: z.string().optional(),
-    url: z.string(),
-    icon: z.string(),
+    url: z.string().optional(),
+    icon: z.string().optional(),
   }),
 });
 
